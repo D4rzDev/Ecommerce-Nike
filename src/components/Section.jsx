2a4d01154c417aa09252868ec26843f9cb3f585b
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2023-10-02 16:11:56 +0800

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,7 +8,7 @@ const Section = () => {
               <h1 className=' text-sm md:text-lg'>2023 Collections</h1>
             <p className=' text-[8px] md:text-xs '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
-            <img className=' h-[230px] pt-4 md:h-[500px] lg:h-[940px] md:w[1220px]' src="/assets/new1.png" alt="" />
+            <img className=' h-[230px] pt-4 md:h-[500px] lg:h-[940px] lg:w-full' src="/assets/new1.png" alt="" />
         </div>
          <img className=' h-[300px] md:h-[580px] lg:h-[1000px]' src="/assets/new2.png" alt="" />
 
